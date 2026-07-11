29920533a6b9dbbd0d9dcd1df93763b918543c9e
vedansh-16 <vedanshjakhmola@gmail.com>
2026-07-11 18:32:07 +0530

    Time: 3 ms (75.52%), Space: 47.4 MB (21.09%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -13,7 +13,10 @@
  *     }
  * }
  */
-class Solution {
+
+ //Approach-1(Inorder traversal + Two Pointers)
+ //Inorder of BST is sorted
+/*class Solution {
     public boolean findTarget(TreeNode root, int k) {
         List<Integer> list = new ArrayList<>();
         fun(root,list);
@@ -38,5 +41,72 @@ class Solution {
         fun(node.left,list);
         list.add(node.val);
         fun(node.right,list);
+    }
+}*/
+class Solution {
+    Stack<TreeNode> asc = new Stack<>();
+    Stack<TreeNode> desc = new Stack<>();
+
+    TreeNode getSmall(){
+        if(asc.isEmpty()){
+            return null;
+        }
+        TreeNode small = asc.peek();
+        asc.pop();
+
+        TreeNode rightChild = small.right;
+        while(rightChild!=null){
+            asc.push(rightChild);
+            rightChild = rightChild.left;
+        }
+        return small;
+    }
+
+    TreeNode getBig(){
+        if(desc.isEmpty()){
+            return null;
+        }
+        TreeNode big = desc.peek();
+        desc.pop();
+
+        TreeNode leftChild = big.left;
+        while(leftChild!=null){
+            desc.push(leftChild);
+            leftChild = leftChild.right;
+        }
+        return big;
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        if(root==null){
+            return false;
+        }
+
+        TreeNode t = root;
+        while(t!=null){
+            asc.push(t);
+            t = t.left;
+        }
+        
+        t=root;
+        while(t!=null){
+            desc.push(t);
+            t = t.right;
+        }
+
+        TreeNode i = getSmall();
+        TreeNode j = getBig();
+
+        while((i!=null&&j!=null) && (i!=j) && (i.val<=j.val)){
+            int sum = i.val + j.val;
+            if(sum==k){
+                return true;
+            }else if(sum>k){
+                j=getBig();
+            }else{
+                i=getSmall();
+            }
+        }
+        return false;
     }
 }
