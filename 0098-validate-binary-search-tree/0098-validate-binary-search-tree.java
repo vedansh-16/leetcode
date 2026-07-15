f1e221288762dd0365fb014b8663c7cee001e751
vedansh-16 <vedanshjakhmola@gmail.com>
2026-07-15 17:35:59 +0530

    Time: 0 ms (100%), Space: 45.1 MB (50.89%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,27 +13,56 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         List<Integer> list = new ArrayList<>();
+//         inorder(root,list);
+
+//         for(int i = 1;i<list.size();i++){
+//             if(list.get(i)<=list.get(i-1)){
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+//     void inorder(TreeNode root,List<Integer> list){
+//         if(root==null){
+//             return;
+//         }
+
+//         inorder(root.left,list);
+//         list.add(root.val);
+//         inorder(root.right,list);
+
+//     }
+// }
+
 class Solution {
+    TreeNode prev = null;
+    boolean ans = true;
     public boolean isValidBST(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        inorder(root,list);
-
-        for(int i = 1;i<list.size();i++){
-            if(list.get(i)<=list.get(i-1)){
-                return false;
-            }
-        }
-
-        return true;
+        inorder(root);
+        return ans;
     }
-    void inorder(TreeNode root,List<Integer> list){
+    void inorder(TreeNode root){
         if(root==null){
             return;
         }
 
-        inorder(root.left,list);
-        list.add(root.val);
-        inorder(root.right,list);
+        inorder(root.left);
+        
+        if(prev==null){
+            prev=root;
+        }else{
+            if(root.val<=prev.val){
+                ans = false;
+                
+            }
+            prev = root;
+        }
+
+        inorder(root.right);
 
     }
 }
