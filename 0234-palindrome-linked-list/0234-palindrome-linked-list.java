35f4dfd526ee3cc2875d5096657e292b8a0e111e
vedansh-16 <vedanshjakhmola@gmail.com>
2026-07-19 19:17:11 +0530

    Time: 3 ms (99.84%), Space: 94.3 MB (79.93%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,23 +8,60 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+//Approach-1(Using extra space)
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         List<Integer> list = new ArrayList<>();
+//         ListNode n = head;
+//         while(n!=null){
+//             list.add(n.val);
+//             n = n.next;
+//         }
+//         int left = 0;
+//         int right = list.size()-1;
+
+//         while(left<=right){
+//             if(list.get(left)!=list.get(right)){
+//                 return false;
+//             }
+//             left++;
+//             right--;
+//         }
+
+//         return true;
+//     }
+// }
+
+//Approach-2(O(1)-space)
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        ListNode n = head;
-        while(n!=null){
-            list.add(n.val);
-            n = n.next;
-        }
-        int left = 0;
-        int right = list.size()-1;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while(left<=right){
-            if(list.get(left)!=list.get(right)){
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode curr = slow;
+        ListNode prev = null;
+
+        while(curr!=null){
+            ListNode nex = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nex;
+        }
+
+        slow = head;
+        curr = prev;
+        while(curr!=null){
+            if(slow.val!=curr.val){
                 return false;
             }
-            left++;
-            right--;
+            slow=slow.next;
+            curr = curr.next;
         }
 
         return true;
