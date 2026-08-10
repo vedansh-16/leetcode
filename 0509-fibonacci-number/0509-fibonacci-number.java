ae88105ba09611473f861ffcc675d2e06481e5d5
vedansh-16 <vedanshjakhmola@gmail.com>
2026-08-10 06:11:26 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.12%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,20 +1,41 @@
+//Memoization
+// class Solution {
+//     HashMap<Integer,Integer> dp = new HashMap<>();
+    // public int fib(int n) {
+    //     if(n==0 || n==1){
+    //         return n;
+    //     }
+
+//         if(dp.containsKey(n)){
+//             return dp.get(n);
+//         }
+
+//         int ans1 = fib(n-1);
+//         int ans2 = fib(n-2);
+
+//         int ans = ans1+ans2;
+        
+//         dp.put(n,ans);
+
+//         return ans;
+//     }
+// }
+
+//Tabulation
 class Solution {
-    HashMap<Integer,Integer> dp = new HashMap<>();
     public int fib(int n) {
         if(n==0 || n==1){
             return n;
         }
 
-        if(dp.containsKey(n)){
-            return dp.get(n);
+        int prev = 1;
+        int prev_prev = 0;
+        int ans=0;
+        for(int i=2;i<=n;i++){
+            ans = prev + prev_prev;
+            prev_prev = prev;
+            prev = ans;
         }
-
-        int ans1 = fib(n-1);
-        int ans2 = fib(n-2);
-
-        int ans = ans1+ans2;
-        
-        dp.put(n,ans);
 
         return ans;
     }
