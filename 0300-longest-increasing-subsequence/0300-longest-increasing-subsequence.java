ddf499d78c977fdbda644919404ad8bdc4a7c28f
vedansh-16 <vedanshjakhmola@gmail.com>
2026-08-14 18:27:06 +0530

    Time: 79 ms (26.3%), Space: 237.5 MB (5.35%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,3 +1,4 @@
+/* Memoization
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
@@ -33,8 +34,32 @@ class Solution {
           
     }
 }
-// class Solution {
-//     public int lengthOfLIS(int[] nums) {
-        
-//     }
-// }
+*/
+// Tabulation
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+         int n = nums.length;
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int prev = -1; prev < i; prev++) {
+
+                if (prev == -1 || nums[prev] < nums[i]) {
+
+                    int c1 = 1 + dp[i + 1][i + 1];
+                    int c2 = dp[i + 1][prev + 1];
+
+                    dp[i][prev + 1] = Math.max(c1, c2);
+
+                } else {
+
+                    dp[i][prev + 1] = dp[i + 1][prev + 1];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
