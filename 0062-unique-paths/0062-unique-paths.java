0e985d2b068d74c4d5ad0900926165a67c8ef54e
vedansh-16 <vedanshjakhmola@gmail.com>
2026-08-16 06:00:57 +0530

    Time: 0 ms (100%), Space: 42.1 MB (66.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,27 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[n+1][m+1];
+
+        dp[n-1][m-1] = 1;
+
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if (i == n - 1 && j == m - 1) {
+                    continue;
+                }
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+
+        return dp[0][0];
+    }
+}
+
+/*Memoization
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[n+1][m+1];
         for(int i=0;i<=n;i++){
             Arrays.fill(dp[i],-1);
         }
@@ -21,4 +42,4 @@ class Solution {
 
         return dp[i][j] = fun(m,n,i+1,j,dp) + fun(m,n,i,j+1,dp);
     }
-}
+}*/
