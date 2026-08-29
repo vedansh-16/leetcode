a0ff3394da4afb605368a5de931d765a2e36c103
vedansh-16 <vedanshjakhmola@gmail.com>
2026-08-29 06:06:08 +0530

    Time: 0 ms (100%), Space: 46.9 MB (80.56%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -13,13 +13,47 @@
  *     }
  * }
  */
+
+ //DFS
+ class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return 1 + Math.max(left,right);
+    }
+ }
+ /*
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root==null) return 0;
 
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        
-        return 1 + Math.max(left,right);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int depth = 0;
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for(int i=0;i<size;i++){
+                TreeNode node = queue.poll();
+                
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
     }
 }
+*/
